7408fb9a1b2574866529dbf597b3a0955d0f7e7a
Enotik98 <poliakitti@gmail.com>
2021-09-26 23:17:37 +0300

    Insert

--- lab_1/lab_1.cpp
+++ lab_1/lab_1.cpp
@@ -10,7 +10,7 @@ private:
         int index;
         U data;
         U time;
-        vector<Node*> Leaves;
+        vector<Node<U>*> Leaves;
         Node(U data, U time);
     };
     int count;
@@ -22,7 +22,8 @@ public:
     Node<T>* getRoot();
     Node<T>* deleteNode();
     void outputTree(Node<T> *current, int temp);
-    
+    void insert(T data, T time);
+    void setIndex(Node<T>* current);
 };
 
 template <typename T>
@@ -48,10 +49,9 @@ typename Tree<T>::template Node<T>* Tree<T>::getRoot(){
     return root;
 }
 template <typename T>
-template <typename U>
-Tree<T>::Node<U>* Tree<T>::deleteNode(){
-    Node<U>* curr = root;
-    Node<U>* prev = root;
+typename Tree<T>::template Node<T>* Tree<T>::deleteNode(){
+    Node<T>* curr = root;
+    Node<T>* prev = root;
     int temp = -1 , dop;
     
     if (root->Leaves.empty()){
@@ -90,5 +90,54 @@ Tree<T>::Node<U>* Tree<T>::deleteNode(){
 }
 template <typename T>
 void Tree<T>::outputTree(Node<T>* current, int temp){
-    cout << current->index << ": " << current << " date";
+    cout << current->index << ": " << current << " data= " << current->data << "|" << " time= " << current->time << "\n";
+    temp++;
+    for(int i = 0;i < current->Leaves.size();i++){
+        for(int j = 0;j < temp; j++){
+            cout << "\t";
+        }
+        cout << i << ". ";
+        outputTree(current->Leaves[i], temp);
+    }
+}
+template <typename T>
+void Tree<T>::insert(T data, T time){
+    Node<T>* curr = root;
+    int temp;
+    if(root->Leaves.empty()){
+        Node<T>* newNode = new Node<T>(data,time);
+        count++;
+        Tree<T>::setIndex(newNode);
+        root->Leaves.push_back(newNode);
+        return ;
+    }
+    
+    while(true){
+        Tree<T>::outputTree(curr);
+        if(curr->Leaves.empty()){
+            Node<T>* newNode = new Node<T>(data, time);
+            count++;
+            Tree<T>::setIndex(newNode);
+            curr->Leaves.push_back(newNode);
+            return;
+        }
+        cout << "Leaves: ";
+        for(int i = 0;i < curr->Leaves.size();i++){
+            cout << i << " ";
+        }
+        cout<<"\nChoose the leap or enter '-1'\n";
+        cin >> temp;
+        if(temp == -1){
+            Node<T>* newNode = new Node<T>(data, time);
+            count++;
+            Tree<T>::setIndex(newNode);
+        }else{
+            curr = curr->Leaves[temp];
+        }
+    }
+    
+}
+template <typename T>
+void Tree<T>::setIndex(Tree<T>::Node<T>* current){
+    current->index = count;
 }
