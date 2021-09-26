fbf536fc55284c55dc5858b5dd1ca249016ae5d2
Enotik98 <poliakitti@gmail.com>
2021-09-26 13:32:45 +0300

    Initial Commit

---
+++ lab_1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  lab_1
+//
+//  Created by Оля on 9/26/21.
+//  Copyright © 2021 Оля. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
